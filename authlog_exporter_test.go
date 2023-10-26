f70a37deda5e7e2cfaedfa98a8e7023148853ad9
woblerr <antkurochkin@gmail.com>
2023-10-26 23:51:13 +0300

    Added the following flags from the exporter-toolkit/web/kingpinflag package (v0.9.1): * `--web.listen-address` * `--web.config.file` * `--web.systemd-socket`
    The `--prom.endpoint` flag has been renamed to `--web.endpoint`.
    The `--prom.port` and `--prom.web-config` flags have been removed.
    
    Also, this update pulled the switching of the package pkg.in/alecthomas/kingpin.v2 v2.2.6 to github.com/alecthomas/kingpin/v2 v2.3.2. This added the ability to specify `no-` prefix for bool parameters.
    
    In service template file variables ware renamed: `ENDPOINT` to `EXPORTER_ENDPOINT`, `PORT` to `EXPORTER_PORT`.

--- authlog_exporter_test.go
+++ authlog_exporter_test.go
@@ -20,7 +20,7 @@ func TestMain(t *testing.T) {
 	port := 50000 + int(n.Int64())
 	os.Args = []string{
 		"authlog_exporter",
-		"--prom.port=" + strconv.Itoa(port),
+		"--web.listen-address=:" + strconv.Itoa(port),
 		"--auth.log",
 		"./test_data/auth.log"}
 	finished := make(chan struct{})
--- promexporter/exporter.go
+++ promexporter/exporter.go
@@ -13,21 +13,23 @@ import (
 )
 
 var (
-	authlogPath       string
-	promPort          string
-	promEndpoint      string
-	promTLSConfigPath string
-	metricHideIP      bool
-	metricHideUser    bool
+	authlogPath    string
+	webFlagsConfig web.FlagConfig
+	webEndpoint    string
+	metricHideIP   bool
+	metricHideUser bool
 )
 
 // SetExporterParams sets path for 'auth.log' from command line argument 'auth.log',
-// HTTP endpoint parameters from command line arguments 'port', 'endpoint' and 'tlsConfigPath'.
-func SetExporterParams(filePath, port, endpoint, tlsConfigPath string, hideIP, hideUser bool) {
+// HTTP endpoint parameters from command line arguments:
+// 'web.endpoint',
+// 'web.listen-address',
+// 'web.config.file',
+// 'web.systemd-socket' (Linux only)'.
+func SetExporterParams(filePath, endpoint string, flagsConfig web.FlagConfig, hideIP, hideUser bool) {
 	authlogPath = filePath
-	promPort = port
-	promEndpoint = endpoint
-	promTLSConfigPath = tlsConfigPath
+	webFlagsConfig = flagsConfig
+	webEndpoint = endpoint
 	metricHideIP = hideIP
 	metricHideUser = hideUser
 }
@@ -35,21 +37,20 @@ func SetExporterParams(filePath, port, endpoint, tlsConfigPath string, hideIP, h
 // Start runs promhttp endpoind and parsing log process.
 func Start(logger log.Logger) {
 	go func(logger log.Logger) {
-		http.Handle(promEndpoint, promhttp.Handler())
+		http.Handle(webEndpoint, promhttp.Handler())
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte(`<html>
 			<head><title>AuthLog exporter</title></head>
 			<body>
 			<h1>AuthLog exporter</h1>
-			<p><a href='` + promEndpoint + `'>Metrics</a></p>
+			<p><a href='` + webEndpoint + `'>Metrics</a></p>
 			</body>
 			</html>`))
 		})
 		server := &http.Server{
-			Addr:              ":" + promPort,
 			ReadHeaderTimeout: 5 * time.Second,
 		}
-		if err := web.ListenAndServe(server, promTLSConfigPath, logger); err != nil {
+		if err := web.ListenAndServe(server, &webFlagsConfig, logger); err != nil {
 			level.Error(logger).Log("msg", "Run web endpoint failed", "err", err)
 			os.Exit(1)
 		}
